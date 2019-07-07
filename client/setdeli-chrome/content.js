a13219f6f6ac871515526f9f84eda1fe14ea592a
thefenderstory <jgrady15@brophybroncos.org>
2019-07-07 09:48:22 -0700

    Create Chrome Extension

---
+++ client/setdeli-chrome/content.js
@@ -0,0 +1,9 @@
+// content.js
+
+var s = document.createElement('script');
+// TODO: add "script.js" to web_accessible_resources in manifest.json
+s.src = chrome.runtime.getURL('inject.js');
+s.onload = function() {
+    this.remove();
+};
+(document.head || document.documentElement).appendChild(s);
